079fc569d40a3e93724fa8418e7f4bb5c26dddf5
kyrgyz-dev <okurman.kenzhegulov@gmail.com>
2025-08-15 16:58:54 +0600

    user/models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.PositiveSmallIntegerField(default=18)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
