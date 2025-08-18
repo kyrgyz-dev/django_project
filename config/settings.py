63dcb5c6c1a6e95388bff4b2c33b514deb81e969
kyrgyz-dev <okurman.kenzhegulov@gmail.com>
2025-08-18 15:40:25 +0600

    models

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # local apps
-    'user.apps.UserConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -79,5 +79,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ product/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
